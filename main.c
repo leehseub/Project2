fa9dc388e344e7bdebcfab4ff96d414fc63105fe
seoljh9423 <seoljh9423@gmail.com>
2025-05-11 16:16:37 +0900

    datecmp timecmp function implement

--- main.c
+++ main.c
@@ -59,10 +59,42 @@ void blockcmp(){
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
-    
+    if (time1->tm_mon < time2->tm_mon){
+        printf("text1 is early\n");
+    }
+    else if (time1->tm_mon > time2->tm_mon){
+        printf("text2 is early\n");
+    }
+    else { //같은 월일 경우 일 비교
+        if (time1->tm_mday < time2->tm_mday){
+            printf("text1 is early\n");
+        }
+        else if (time1->tm_mday > time2->tm_mday){
+            printf("text2 is early\n");
+        }
+        else{
+            printf("same date\n");
+        }
+    }
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(){
-    
+    if (time1->tm_hour < time2->tm_hour){
+        printf("text1 is early\n");
+    }
+    else if (time1->tm_hour > time2->tm_hour){
+        printf("text2 is early\n");
+    }
+    else { // 같은 시일 경우 분 비교
+        if (time1->tm_min < time2->tm_min){
+            printf("text1 is early\n");
+        }    
+        else if (time1->tm_min > time2->tm_min){
+            printf("text2 is early\n");
+        }
+        else{
+            printf("same time\n");
+        }
+    }
 }
